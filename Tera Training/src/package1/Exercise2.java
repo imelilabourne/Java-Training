5fa43225540698b0cadbbd8bbd2b6b21a7102d27
Tera <elilabourne.detorres@gmail.com>
2020-11-24 15:54:57 +0800

    Speed Converter

---
+++ Tera Training/src/package1/Exercise2.java
@@ -0,0 +1,37 @@
+package package1;
+
+public class Exercise2 {
+
+	public static void main(String[] args) {
+		printConversion(7);
+	}
+	
+	
+	public static long toMilesPerHour(double kilometersPerHour){
+
+	    
+	    if(kilometersPerHour < 0){
+	        return -1;
+	    }
+	    
+	    else{
+	        long temp = Math.round(kilometersPerHour );
+	        return temp;
+	    }
+	}
+
+	public static void printConversion(double kilometersPerHour){
+	    
+	   if(kilometersPerHour < 0){
+	       System.out.println("Invalid Value");
+	       
+	   }
+	   else{
+		   
+		    long finalAns = toMilesPerHour(kilometersPerHour * 0.62137);
+		    System.out.println(kilometersPerHour + " km/h = " + finalAns + " mi/h");
+	   
+	   }
+	}
+
+}
