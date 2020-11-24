710a2d3efc9b83adde9a484228ddf2ea8d426658
Tera <elilabourne.detorres@gmail.com>
2020-11-24 14:21:39 +0800

    java methods

--- Tera Training/src/package1/Exercise1.java
+++ Tera Training/src/package1/Exercise1.java
@@ -3,8 +3,36 @@ package package1;
 public class Exercise1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.print("Hello, I am Eli Labourne");
+		
+		
+		
+		int highScore = calcultedHighScorePosition(900);
+		
+		displayHighScorePosition("eli", highScore);
+		
+	
+	}
+	
+	public static void displayHighScorePosition(String name, int position){
+		System.out.println(name + " managed to get into position " + position
+				+ " on the high score table");
+	}
+
+	
+	public static int calcultedHighScorePosition(int score) {
+		if(score > 1000) {
+			return 1;
+		}
+		else if(score > 500 && score < 1000) {
+			return 2;
+		}
+		else if(score > 100 && score < 500) {
+			return 3;
+		}
+		
+		else {
+			return 4;
+		}
 	}
 
 }
